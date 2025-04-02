faf151484ed4e22fa41eb875f5c5b0a3c2136e97
ZbigniewCement <177277@stud.prz.edu.pl>
2025-04-02 14:29:49 +0000

    3_2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,19 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-
-// import org.junit.jupiter.api.Test;
-
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+    public static void main(String[] args) {
+                
+    for (int z = 0; z < 3; z++) {
+        int gwiazdki = 7;
+        for (int i = 0; i < 4; i++) {  
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < gwiazdki; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
 
-  // @Test
-  // void addition() {
-  //     assertEquals(2, 1 + 1);
-  // }
+            gwiazdki -= 2;
+            }
+        }     
+    }   
 }
